3084bd2b081150136815d60eb22a1af97d3ec4c3
Bob Copeland <me@bobcopeland.com>
2014-03-25 22:47:10 -0400

    wcn36xx: don't set link state to idle
    Once that happens, we are unable to successfully remove any
    stations.  Taking this out allows mesh leave/join to work.
    
    Signed-off-by: Bob Copeland <me@bobcopeland.com>

--- main.c
+++ main.c
@@ -797,8 +797,6 @@ static void wcn36xx_bss_info_changed(struct ieee80211_hw *hw,
 						link_state);
 		} else {
 			wcn36xx_smd_send_beacon(wcn, vif, NULL, tim_off, 0);
-			wcn36xx_smd_set_link_st(wcn, vif->addr, vif->addr,
-						WCN36XX_HAL_LINK_IDLE_STATE);
 		}
 	}
 out:
